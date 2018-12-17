c22f1c3641341d957709fb80476122146c84d357
Miles Watson <32907412+mileswatson@users.noreply.github.com>
2018-12-17 13:01:13 +0000

    Updated Program.cs to more recent REPL

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using SharpLearningKit;
 
 namespace Demo
@@ -7,91 +7,125 @@ namespace Demo
     {
         static void Clear()
         {
-            for (int i = 0; i < 50; i++)
-            {
-                Console.WriteLine();
-            }
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         }
 
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             string userAnswer;
             Model m;
             Loader l;
             Parser p;
             Matrix questions, answers;
-            string path = "C:/Users/miles/Documents/Programming/sharp-learning-kit/data/";
+            Clear();
             while (true)
             {
                 while (true)
                 {
-                    Clear();
                     try
                     {
                         Console.WriteLine("ACTION:");
-                        Console.WriteLine("\tNEW - create new model");
-                        Console.WriteLine("\tLOAD - load previous model");
+                        Console.WriteLine("\tCREATE");
+                        Console.WriteLine("\tLOAD");
+                        Console.WriteLine("\tEXIT");
                         userAnswer = Console.ReadLine().ToUpper();
                         Clear();
-                        if (userAnswer == "NEW")
+                        if (userAnswer == "CREATE")
                         {
                             Console.WriteLine("MODEL STRUCTURE:");
                             m = new Model(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
                             Clear();
                             Console.WriteLine("NAME:");
-                            l = new Loader(path+Console.ReadLine().ToLower() + ".slkm");
-                            l.Save(m);
+                            l = new Loader(Console.ReadLine().ToLower() + ".slkm");
+                            Clear();
                             break;
                         }
                         else if (userAnswer == "LOAD")
                         {
                             Console.WriteLine("NAME:");
-                            l = new Loader(path+Console.ReadLine().ToLower() + ".slkm");
+                            l = new Loader(Console.ReadLine().ToLower() + ".slkm");
+                            Clear();
                             m = l.Load();
                             break;
+                        }
+                        else if (userAnswer == "EXIT")
+                        {
+                            Clear();
+                           return 0;
                         }
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine(e);
+                        Clear();
                     }
                 }
                 while (true)
                 {
-                    Clear();
-                    try
-                    {
-                        Console.WriteLine("CSV:");
-                        p = new Parser(2, 2);
-                        (questions, answers) = p.Load(path+Console.ReadLine().ToLower() + ".csv");
-                        break;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                    }
-                }
-                /*
-                while (true)
-                {
-                    Clear();
                     try
                     {
                         Console.WriteLine("ACTION:");
                         Console.WriteLine("\tTRAIN");
                         Console.WriteLine("\tPREDICT");
                         Console.WriteLine("\tEVALUATE");
+                        Console.WriteLine("\tSAVE");
+                        Console.WriteLine("\tBACK");
+                        userAnswer = Console.ReadLine().ToUpper();
+                        Clear();
+                        if (userAnswer == "TRAIN")
+                        {
+                            p = new Parser(m.synapses[0].numRows,m.synapses[m.synapses.Length-1].numColumns);
+                            Console.WriteLine("CSV:");
+                            (questions, answers) = p.Load(Console.ReadLine().ToLower()+".csv");
+                            Clear();
+                            Console.WriteLine("ITERATIONS:");
+                            m.Train(questions, answers, Int32.Parse(Console.ReadLine()));
+                            Clear();
+                        }
+                        else if (userAnswer == "PREDICT")
+                        {
+                            p = new Parser(m.synapses[0].numRows,0);
+                            Console.WriteLine("CSV:");
+                            (questions, answers) = p.Load(Console.ReadLine().ToLower()+".csv");
+                            Clear();
+                            Console.WriteLine("DECIMAL PLACES:");
+                            userAnswer = Console.ReadLine();
+                            Clear();
+                            Console.WriteLine( m.Predict(questions).Round(Int32.Parse(userAnswer)) );
+                            Console.ReadLine();
+                            Clear();
+                        }
+                        else if (userAnswer == "EVALUATE")
+                        {
+                            p = new Parser(m.synapses[0].numRows,m.synapses[m.synapses.Length-1].numColumns);
+                            Console.WriteLine("CSV:");
+                            (questions, answers) = p.Load(Console.ReadLine().ToLower()+".csv");
+                            Clear();
+                            Console.WriteLine("DECIMAL PLACES:");
+                            userAnswer = Console.ReadLine();
+                            Clear();
+                            Console.WriteLine( answers );
+                            Console.WriteLine( m.Predict( questions ).Round(Int32.Parse(userAnswer)) );
+                            Console.ReadLine();
+                            Clear();
+                        }
+                        else if (userAnswer == "SAVE")
+                        {
+                            l.Save(m);
+                            Clear();
+                        }
+                        else if (userAnswer == "BACK")
+                        {
+                            break;
+                        }
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine(e);
+                        Clear();
                     }
                 }
-                */
-                m.Train(questions, answers, 10000);
-                l.Save(m);
-                Console.WriteLine( m.Predict( questions ) );
-                Console.ReadLine();
+                
             }
         }
     }
